20bc22eec812125b7a3b9b8e7676191dd03f6125
yankelevich-dima <yankelevich.dima@gmail.com>
2017-01-21 15:14:47 +0300

    fixed variable name

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import json
 
 with open('config.json', 'r') as jsonfile:
-    data = json.load(jsonfile)
+    config_data = json.load(jsonfile)
 
-TOPICS_MAP = {element['slug']: element['title'] for element in data['topics']}
-ARTICLES = data['articles']
+TOPICS_MAP = {element['slug']: element['title'] for element in config_data['topics']}
+ARTICLES = config_data['articles']
 
 ARTICLES_PATH = './articles/'
 TEMPLATE_PATH = './templates/'
